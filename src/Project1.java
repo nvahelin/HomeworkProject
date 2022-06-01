813ba876dfe6da759ada0e2e8fe1b8e56eadba54
nvahelin@gmail.com <nvahelin@gmail.com>
2022-05-31 22:16:23 -0400

    1st commit

---
+++ src/Project1.java
@@ -0,0 +1,21 @@
+
+public class Project1 {
+
+	public static void main(String[] args) {
+		
+		
+		int a=5;
+		int b=4;
+		int temp;
+		
+		temp=a;
+		a=b;
+		b=a;
+		b=temp;
+		
+		System.out.println("a : " +a);
+		System.out.println("b : " +b);
+
+	}
+
+}
